61dd1f0a68f6f10a99281627b78f9f12c3d6f3fb
CoderDJ <85845460+coderdevang@users.noreply.github.com>
2021-06-17 17:13:16 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# calculator
+#input
+a = float(input("first number : "))
+b = float(input("second number : "))
+#additon
+additon = a + b
+#subtraction
+subtraction = a - b
+#multiplication
+multiplication = a*b
+#divisor
+divisor = b
+dividened = a
+quotiont = dividened / divisor
+d = dividened //divisor
+print(quotiont)
+#Answers
+print(f'''Additon : {additon}
+Subtraction : {subtraction}
+Divison : {quotiont}
+Multiplication : {multiplication}''')
